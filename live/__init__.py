5072df2d2372af0c0c859590bb4f15551e8c635c
Sven Schultze <schultze.sven@googlemail.com>
2020-10-28 18:12:07 +0100

    Implement automatic Figure creation on imshow for current cell

--- live/__init__.py
+++ live/__init__.py
@@ -1,10 +1,31 @@
 import pkgutil
 from IPython.display import display, Javascript
+import inspect
 
 from live.figure import Figure
 
 
 broadcast_template = pkgutil.get_data(__name__, "templates/broadcast.js").decode("utf-8")
+
+figures = {}
+
+def figure(name=None, width=30):
+    context = get_current_context()
+    if context not in figures.keys():
+        figures[context] = Figure(name, width)
+
+    return figures[context]
+
+def imshow(img, width=30):
+    fig = figure(width=width)
+    fig.imshow(img)
+
+def get_current_context():
+    for frame in inspect.stack():
+        if "ipython-input" in frame.filename:
+            return inspect.stack()[0].filename
+    
+    return None
 
 def broadcast(channel, message):
     js = broadcast_template.replace(
@@ -14,4 +35,3 @@ def broadcast(channel, message):
     )
 
     display(Javascript(js))
-
