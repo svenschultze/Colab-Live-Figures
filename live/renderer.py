24e5cab19c620c99c69d6851cfc2309a0134a128
Sven Schultze <schultze.sven@googlemail.com>
2020-10-30 01:21:50 +0100

    Fix small error in renderer

--- live/renderer.py
+++ live/renderer.py
@@ -1,21 +1,21 @@
 from google.colab.output._js_builder import Js
 
-listen = Js(f"""
-    function(channel, width) {{
+listen = Js("""
+    function(channel, width) {
         elem = document.createElement("img")
         elem.style.imageRendering = "pixelated"
         elem.style.width = width + "%"
         elem.style.float = "left"
         output = google.colab.output.getActiveOutputArea()
         output.appendChild(elem)
-        new BroadcastChannel('test').onmessage = (msg) => {{
+        new BroadcastChannel(channel).onmessage = (msg) => {
             elem.src = msg.data
-        }}
-    }}
+        }
+    }
 """)
 
-broadcast = Js(f"""
-    function(channel, msg) {{
+broadcast = Js("""
+    function(channel, msg) {
         new BroadcastChannel(channel).postMessage(msg)
-    }}
+    }
 """)
