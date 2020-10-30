767a52629518c390bc8bc86db9b9e1c28fbb3143
Sven Schultze <schultze.sven@googlemail.com>
2020-10-30 01:32:46 +0100

    Fix bug in renderer for multiple figures in one cell

--- live/renderer.py
+++ live/renderer.py
@@ -2,11 +2,12 @@ from google.colab.output._js_builder import Js
 
 listen = Js("""
     function(channel, width) {
-        elem = document.createElement("img")
+        let elem = document.createElement("img")
         elem.style.imageRendering = "pixelated"
         elem.style.width = width + "%"
         elem.style.float = "left"
-        output = google.colab.output.getActiveOutputArea()
+        elem.style.margin = "5px"
+        let output = google.colab.output.getActiveOutputArea()
         output.appendChild(elem)
         new BroadcastChannel(channel).onmessage = (msg) => {
             elem.src = msg.data
